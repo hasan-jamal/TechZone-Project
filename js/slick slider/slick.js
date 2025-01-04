38580c8b811b1d7d9075e95b60fc3e301fad8dbc
Hasan Jamal <hasanjamalabusharekh@gmail.com>
2025-01-04 09:53:54 +0200

    Update

--- js/slick slider/slick.js
+++ js/slick slider/slick.js
@@ -1,3 +1,37 @@
+// StartSlider 1
+$(document).ready(function () {
+  $(".slider-items").slick({
+    prevArrow: $(".left-items"),
+    nextArrow: $(".right-items"),
+    centerMode: true,
+    centerPadding: "50px",
+    infinite: true,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1025, // Mobile landscape and larger
+        settings: {
+          slidesToShow: 5, // Number of slides to show at once
+          centerMode: true, // Optional: Keep centering on smaller screens
+          centerPadding: "0px", // Adjust padding if needed
+        },
+      },
+      {
+        breakpoint: 614.5, // Mobile portrait and smaller
+        settings: {
+          slidesToShow: 3, // Number of slides to show at once
+          centerMode: false, // Optional: Turn off centering for small screens
+          centerPadding: "0px", // Adjust padding if needed
+        },
+      },
+    ],
+  });
+});
+//  End Slider 1
 // StartSlider 2
 
 $(document).ready(function () {
